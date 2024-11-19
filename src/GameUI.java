1c65b00410c6db813143d04af2efaf50340120a9
mhultjavad24 <mhultjavad24@proton.me>
2024-11-19 13:16:06 +0100

    Create example methods for rendering UI based on game data

--- src/GameUI.java
+++ src/GameUI.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class GameUI extends JFrame {
     private JPanel mainPanel = new JPanel(new BorderLayout());
@@ -8,21 +9,59 @@ public class GameUI extends JFrame {
 
 
     public GameUI() {
+//        setTitle("DarkGreen Quiz");
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setSize(800, 600);
+//        JButton Catagory1 = new JButton("Catagory 1");
+//        JButton Catagory2 = new JButton("Catagory 2");
+//        JButton Catagory3 = new JButton("Catagory 3");
+//        JButton ChooseCatagory = new JButton("Choose Catagory");
+//        CatagoryPanel.add(ChooseCatagory);
+//        CatagoryPanel.add(Catagory1);
+//        CatagoryPanel.add(Catagory2);
+//        CatagoryPanel.add(Catagory3);
+//        mainPanel.add(CatagoryPanel, BorderLayout.CENTER);
+//        add(mainPanel);
+//        setLocationRelativeTo(null);
+//        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        GameUI gameUI = new GameUI();
+    }
+
+    public void renderCategorySelection(List<Category> categories) {
         setTitle("DarkGreen Quiz");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //setSize(800, 600);
-        pack();
-        JButton Catagory1 = new JButton("Catagory 1");
-        JButton Catagory2 = new JButton("Catagory 2");
-        JButton Catagory3 = new JButton("Catagory 3");
+        setSize(800, 600);
         JButton ChooseCatagory = new JButton("Choose Catagory");
         CatagoryPanel.add(ChooseCatagory);
-        CatagoryPanel.add(Catagory1);
-        CatagoryPanel.add(Catagory2);
-        CatagoryPanel.add(Catagory3);
+        for (Category category : categories) {
+            JButton button = new JButton(category.getName());
+            CatagoryPanel.add(button);
+        }
         mainPanel.add(CatagoryPanel, BorderLayout.CENTER);
         add(mainPanel);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
+    public void renderQuestion(Question question) {
+        setTitle("DarkGreen Quiz");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        JButton ChooseCatagory = new JButton(question.getText());
+        CatagoryPanel.add(ChooseCatagory);
+
+        for (String answer : question.answers) {
+            JButton button = new JButton(answer);
+            CatagoryPanel.add(button);
+        }
+        mainPanel.add(CatagoryPanel, BorderLayout.CENTER);
+        add(mainPanel);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         GameUI gameUI = new GameUI();
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Programming"));
+        categories.add(new Category("Animals and Nature"));
+        Category c1 = new Category("History");
+        categories.add(c1);
+
+        String[] answers = {"1939", "1940", "1941", "1942"};
+        Question question = new Question("When did second world war start?", c1, answers, 0);
+
+//        gameUI.renderCategorySelection(categories);
+//
+        gameUI.renderQuestion(question);
     }
 }
