069bb0a2dd1d5d02647ae295f7283836ba39d093
Maria J Kirkou <mariajamalaki@hotmail.com>
2024-11-26 10:37:36 +0100

    ny metod för ändring av score DEL2

--- src/GameUI.java
+++ src/GameUI.java
@@ -18,6 +18,26 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameUI extends JFrame implements ActionListener {
     private List<Category> categories;
     private List<Question> questions;
